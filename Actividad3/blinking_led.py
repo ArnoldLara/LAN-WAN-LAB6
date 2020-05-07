3a8b6a73e198492aee3b7cec8b4402e20d3f4d3a
ArnoldLara <you@example.com>
2020-05-07 14:22:05 -0500

    Cambios

--- Actividad3/blinking_led.py
+++ Actividad3/blinking_led.py
@@ -5,18 +5,21 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 
+ledGreen = 8
+ledRed = 10
+
 def led(color, state):
     if color == 'RED':
         if state == 'ON':
-            GPIO.output(8, GPIO.HIGH) # Turn on led green
-            GPIO.output(10, GPIO.LOW) # Turn off led red
+            GPIO.output(ledRed, GPIO.HIGH) # Turn on led green
+            GPIO.output(ledGreen, GPIO.LOW) # Turn off led red
         else:
-            GPIO.output(8, GPIO.LOW) # Turn off led red
+            GPIO.output(ledRed, GPIO.LOW) # Turn off led red
 
     elif color == 'GREEN':
         if state == 'ON':
-            GPIO.output(8, GPIO.LOW) # Turn on led green
-            GPIO.output(10, GPIO.HIGH) # Turn off led red
+            GPIO.output(ledRed, GPIO.LOW) # Turn on led green
+            GPIO.output(ledGreen, GPIO.HIGH) # Turn off led red
         else:
-            GPIO.output(10, GPIO.LOW) # Turn off led red
+            GPIO.output(ledGreen, GPIO.LOW) # Turn off led red
     return 'OK'

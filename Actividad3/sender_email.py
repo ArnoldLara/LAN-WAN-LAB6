02b9ec3aa0890371ba28b849fe51c6e5997cf1ad
ArnoldLara <you@example.com>
2020-05-07 11:41:00 -0500

    Se organizaon archivos en carpetas de activiad. Se implementa un enviador de emails de acuerdo a la evaluacion de los signos vitales.

---
+++ Actividad3/sender_email.py
@@ -0,0 +1,33 @@
+import smtplib, ssl
+
+port = 587  # For startTLS
+smtp_server = "smtp.gmail.com"
+sender_email = "lanwan202010@gmail.com"  # Enter your address
+password = "linux_Pass"
+
+
+
+def envio_email(receiver_email_attr, gluc_attr, temp_attr, spo2_attr):
+    receiver_email = receiver_email_attr
+    gluc = gluc_attr
+    temp = temp_attr
+    spo2 = spo2_attr
+    # message = """\
+    # Subject: Alerta de signos vitales
+    #
+    # This message is sent from Python."""
+
+    SUBJECT = 'Alerta de signos vitales'
+    TEXT=gluc+'\n'+temp+'\n'+spo2
+
+    message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
+
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        print(server.login(sender_email, password))
+        print(server.sendmail(sender_email, receiver_email, message))
+    return 'OK'
--- Actividad3/vitals_notification.py
+++ Actividad3/vitals_notification.py
@@ -1,6 +1,7 @@
 #Include the Beebotte SDK for Python
 from beebotte import *
 import statistics
+import sender_email
 
 bclient = BBT("RTuCupjAtBf6Ica9wLSXIwWi", "XfVtQRTeiE5jwjz4eUBchXuUTBZHbRpY")
 
@@ -9,7 +10,11 @@ rec1 = Resource(bclient,'Signos_vitales','Glucosa')
 rec2 = Resource(bclient,'Signos_vitales','Temperatura')
 rec3 = Resource(bclient,'Signos_vitales','Oxigeno')
 
-
+if input("Desea recibir un email por estado de alerta: (S)(N)") == 'S':
+    receiver_email=input("Ingrese su correo electronico: ")
+    email=True
+else:
+    email=False
 
 while True:
     gluc_list = rec1.read(limit = 5)
@@ -24,6 +29,7 @@ while True:
     print('Glucosa:', gluc[1],'mg/dl ','Temperatura:',"%.2f" % temp[1],'°','Oxigeno:',oxi[1],'%')
     print('Glucosa:', gluc[0],'mg/dl ','Temperatura:',"%.2f" % temp[0],'°','Oxigeno:',oxi[0],'%')
 
+    alerta=False
     glucosa=0
     gluc_mean = statistics.mean(gluc)
 
@@ -32,17 +38,19 @@ while True:
     elif gluc_mean >=108 and gluc_mean <= 125:
         glucosa = 'Glucosa: Prediabetes( 108 – 125 mg/dl )'
     elif gluc_mean >=126:
+        alerta=True
         glucosa = 'Glucosa: Diabetes( < 126 mg/dl)'
 
     temperatura=0
     temp_mean = statistics.mean(temp)
     #print(temp_mean)
     if temp_mean >= 35 and temp_mean <= 37:
-        temperatura = 'Temperatura: Normal (35° – 37°)'
+        temperatura = 'Temperatura: Normal (35 – 37 grados centigrados)'
     elif temp_mean >= 37.1 and temp_mean <= 37.9:
-        temperatura = 'Temperatura: Estado febril (37.1° - 37.9 °)'
+        temperatura = 'Temperatura: Estado febril (37.1 - 37.9 grados centigrados)'
     elif temp_mean >=38:
-        temperatura = 'Temperatura: Fiebre (38° - 40°)'
+        alerta=True
+        temperatura = 'Temperatura: Fiebre (38 - 40 grados centigrados)'
 
     oxigeno=0
     oxi_mean = statistics.mean(oxi)
@@ -51,9 +59,18 @@ while True:
     elif oxi_mean >= 91 and oxi_mean <= 94:
         oxigeno = 'SPO2: Bajo nivel(91% - 94%)'
     elif oxi_mean <=90:
+        alerta=True
         oxigeno = 'SPO2: Problemas de respiración o circulación (< 90%)'
 
     print(glucosa, temperatura, oxigeno)
+
+
+    if email and alerta:
+        sender_email.envio_email(receiver_email,glucosa,temperatura,temperatura)
+        break
+    elif alerta:
+        print('ALERTA HAY UNA CONDICIÓN CRITICA.')
+        alerta = False
 
 
 
